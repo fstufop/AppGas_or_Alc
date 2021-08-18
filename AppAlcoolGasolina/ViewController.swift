fde76e927ea0223b8d62c63b1257c173a1319194
fstufop <fstufop@gmail.com>
2021-08-18 17:14:34 -0300

    Iniciando o código

--- AppAlcoolGasolina/ViewController.swift
+++ AppAlcoolGasolina/ViewController.swift
@@ -9,8 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+       @IBOutlet weak var alcoolPriceField: UITextField!
+       @IBOutlet weak var gasPriceField: UITextField!
+       @IBOutlet weak var resultField: UILabel!
+     var test: Float = 0.0
+    let alcoolPrice = alcoolPriceField.text{
+        
+    }
+    let alcoolPrice = gasPriceField.text{
+        
+    }
+       test = alcoolPrice/gasPrice
+       @IBAction func teste(_ sender: Any) {
+           
+           if (alcoolPrice < 0.76)
+           result = "Abasteça com Álcool"
+       }
+        else if (test < 0.76){
+           result = "Abasteça com Álcool"
+       }
+        else{
+           result = "A relação preço/rendimento está a mesma"
+       }
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
